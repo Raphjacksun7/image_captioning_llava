11993350e3a907b12ba1c5b3f580fb8fa5d7b3eb
Raphael AVOCEGAMOU <avocegamouraph@gmail.com>
2024-05-21 09:24:11 -0400

    new changes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+// module.exports = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'avatars.githubusercontent.com',
+//         port: '',
+//         pathname: '**'
+//       }
+//     ]
+//   }
+// }
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -9,5 +21,17 @@ module.exports = {
         pathname: '**'
       }
     ]
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Setting up fallbacks for Node.js modules
+      config.resolve.fallback = {
+        ...config.resolve.fallback, // Copy existing fallbacks
+        crypto: require.resolve('crypto-browserify'), // Alias 'crypto' to 'crypto-browserify'
+      };
+    }
+    return config;
   }
-}
+};
+
+module.exports = nextConfig;
